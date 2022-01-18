42d1cb48428c3bbf3d0a3f7dc0765f26516e48bd
Marya Samsonenko <samashushka7@gmail.com>
2022-01-18 18:00:46 +0200

    add part 4

--- script.js
+++ script.js
@@ -167,36 +167,139 @@ P.S. Функции вызывать не обязательно*/
 
 // =================================================================================
 
-const options = {
-  name: "test",
-  width: 1024,
-  heigth: 1024,
-  color: {
-    border: "black",
-    bg: "red",
+// const options = {
+//   name: "test",
+//   width: 1024,
+//   heigth: 1024,
+//   color: {
+//     border: "black",
+//     bg: "red",
+//   },
+//   makeTest: function () {
+//     console.log("Test");
+//   },
+// };
+// options.makeTest();
+
+// const { border, bg } = options.color;
+
+// let counter = 0;
+
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i}  имеет значение ${options[key][i]}`);
+//       counter++;
+//     }
+//   } else {
+//     console.log(`Свойство ${key}  имеет значение ${options[key]}`);
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+// console.log(Object.keys(options));
+// console.log(Object.keys(options).length);
+
+// =========================================================================
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+// Код возьмите из предыдущего домашнего задания
+
+// ========================================================================================
+
+const personalMovieDB = {
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+  start: function () {
+    personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (
+      personalMovieDB.count == "" ||
+      personalMovieDB.count == null ||
+      isNaN(personalMovieDB.count)
+    ) {
+      personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
   },
-  makeTest: function () {
-    console.log("Test");
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+      } else {
+        console.log("error");
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMovieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+      console.log("Вы киноман");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  toggleVisibleMyDB: function () {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 1; i < 2; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+      if (genre === "" || genre == null) {
+        console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre;
+      }
+
+      // Альтернативный вариант из урока
+
+      // let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+      // if (genres === '' || genres == null) {
+      //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+      //     i--;
+      // } else {
+      //     personalMovieDB.genres = genres.split(', ');
+      //     personalMovieDB.genres.sort();
+      // }
+    }
+
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
   },
 };
-options.makeTest();
-
-const { border, bg } = options.color;
-
-let counter = 0;
-
-for (let key in options) {
-  if (typeof options[key] === "object") {
-    for (let i in options[key]) {
-      console.log(`Свойство ${i}  имеет значение ${options[key][i]}`);
-      counter++;
-    }
-  } else {
-    console.log(`Свойство ${key}  имеет значение ${options[key]}`);
-    counter++;
-  }
-}
-console.log(counter);
-
-console.log(Object.keys(options));
-console.log(Object.keys(options).length);
