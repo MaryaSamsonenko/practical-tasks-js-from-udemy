15ea1f938f1b5df0651ff05cbac905b2e06d3e8c
Marya Samsonenko <samashushka7@gmail.com>
2022-02-03 16:56:15 +0200

    add modal task 3

--- js/script.js
+++ js/script.js
@@ -78,4 +78,52 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   setClock(".timer", deadline);
+
+  // Modal ======================================
+
+  const modalTrigger = document.querySelectorAll("[data-modal]");
+  const modal = document.querySelector(".modal");
+  const modalClose = document.querySelector("[data-close]");
+
+  function openModal(params) {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    //   modal.classList.toggle("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+  function closeModal(params) {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    // modal.classList.toggle("hide");
+    document.body.style.overflow = "";
+  }
+  modalClose.addEventListener("click", closeModal);
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 10000);
+  function showModalByScroll(params) {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
 });
